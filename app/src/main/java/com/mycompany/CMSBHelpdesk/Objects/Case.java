ffa0029d0f5db39f837511beee392990e6cfeb08
abel <abelhii@gmail.com>
2015-06-23 16:24:07 +0800

    case
    23/06/2015
    -Abel Hii

---
+++ app/src/main/java/com/mycompany/CMSBHelpdesk/Objects/Case.java
@@ -0,0 +1,52 @@
+package com.mycompany.CMSBHelpdesk.objects;
+
+/**
+ * Created by Abel on 09/06/2015.
+ */
+public class Case {
+    private String username ;
+    private String description;
+    private String status;
+    private String id;
+    private String sync;
+
+    public Case(String id, String username, String description, String status, String sync){
+        this.id = id;
+        this.username = username;
+        this.description = description;
+        this.status = status;
+        this.sync = sync;
+    }
+
+    //GETTERS:
+    public String getID(){
+        return id;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public String getStatus(){
+        return status;
+    }
+    public String getSync(){
+        return sync;
+    }
+
+    //SETTERS:
+    public void setID(String id){
+        this.id = id;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+}
